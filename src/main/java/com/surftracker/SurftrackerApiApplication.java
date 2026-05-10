948b706c8258efd5fb6e7a8c5059059b575c7fa2
leoben25 <147931123+leoben25@users.noreply.github.com>
2026-05-09 20:35:22 -0500

    Initial commit - proyecto base SurfTracker

---
+++ src/main/java/com/surftracker/SurftrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.surftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurftrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurftrackerApiApplication.class, args);
+	}
+
+}
---
+++ surftracker-API/src/test/java/com/surftracker/SurftrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surftracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurftrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
